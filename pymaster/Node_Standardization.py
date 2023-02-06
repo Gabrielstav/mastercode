70cb76d14322beda3529d796958e404ec6add12b
Gabrielstav <gabrielstav@yahoo.no>
2023-02-06 17:09:38 +0100

    staticmethod class draft

--- pymaster/Node_Standardization.py
+++ pymaster/Node_Standardization.py
@@ -1,13 +1,102 @@
+import pathlib
 from dataclasses import dataclass
 import pandas as pd
 import numpy as np
 import pybedtools as pbt
 import os as os
-from Processing import Node
-from Processing import Celline
-from Processing import Cellines
+
+
 
 # I could see all of this being in a class called "Pre-processing"
+
+
+# OK, make everything into a static class called Pipeline
+
+class Pipeline:
+
+    @staticmethod
+    def default_path_to_raw_data(*args):
+        default_path_to_raw_data_directory = "/Users/GBS/Master/Pipeline/INC-tutorial/hicpro_results/hic_results/matrix/chr18/raw/50000"
+        return os.path.join(default_path_to_raw_data_directory, *args)
+
+    @staticmethod
+    def default_path_to_output(*args):
+        default_path_to_output_directory = "/Users/GBS/Master/Pipeline/python_pipe_test/bedpe_testing"
+        return os.path.join(default_path_to_output_directory, *args)
+
+    @staticmethod
+    def read_hic_output(matrix_file, bed_file):
+        matrix_df = pd.read_csv(matrix_file, sep="\t", header=None)
+        bed_df = pd.read_csv(bed_file, sep="\t", header=None)
+        return matrix_df, bed_df
+
+    
+
+
+Pipeline.default_path_to_raw_data("chr18_50000.matrix")
+
+
+# @dataclass(frozen=True, eq=True) #doesnt make sense to use dataclasses here
+# class Pipeline:
+#
+#
+#     @classmethod
+#     def from_default_raw(cls):
+#         return cls.from_dir("/Users/GBS/Master/Pipeline/INC-tutorial/hicpro_results/hic_results/matrix/chr18/raw/50000")
+#
+#     @classmethod
+#     def from_default_output(cls):
+#         return cls.from_dir("/Users/GBS/Master/Pipeline/python_pipe_test/bedpe_testing")
+#
+#     @classmethod
+#     def from_dir(cls, param):
+#         pass
+#
+#     @classmethod
+#     def instantiate(cls):
+#         for file in os.listdir(self.rawdata):
+#             if file.endswith(".matrix"):
+#                 matrix_file = os.path.join(self.rawdata, file)
+#             elif file.endswith(".abs.bed"):
+#                 bed_file = os.path.join(self.rawdata, file)
+#         return Pipeline(matrix_file, bed_file)
+#
+#     def read_hic_output(self):
+#         matrix_file = os.path.join(self.rawdata, "chr18_50000.matrix")
+#         bed_file = os.path.join(self.rawdata, "chr18_50000_abs.bed")
+#
+#         matrix_df = pd.read_csv(matrix_file, sep="\t", header=None)
+#         bed_df = pd.read_csv(bed_file, sep="\t", header=None)
+#
+#         return matrix_df, bed_df
+#
+#     def write_hic_output(self):
+#         matrix_df, bed_df = self.read_hic_output()
+#         matrix_path = matrix_df.to_csv(os.path.join(self.output, "chr18_50000.matrix"), sep="\t", header=None, index=False)
+#         bed_path = bed_df.to_csv(os.path.join(self.output, "chr18_50000_abs.bed"), sep="\t", header=None, index=False)
+#
+#         return matrix_path, bed_path
+#
+#     def make_bedpe(self):
+#         matrix_df, bed_df = self.read_hic_output()
+#         bedpe_df = pd.DataFrame()
+#         bedpe_df["chr1"] = bed_df[0]
+#         bedpe_df["start1"] = bed_df[1]
+#         bedpe_df["end1"] = bed_df[2]
+#         bedpe_df["chr2"] = bed_df[0]
+#         bedpe_df["start2"] = bed_df[1]
+#         bedpe_df["end2"] = bed_df[2]
+#         bedpe_df["score"] = matrix_df[3]
+#
+#         return bedpe_df
+#
+#     def write_bedpe(self):
+#         bedpe_df = self.make_bedpe()
+#         bedpe_path = bedpe_df.to_csv(os.path.join(self.output, "chr18_50000.bedpe"), sep="\t", header=None, index=False)
+#
+#         return bedpe_path
+#
+
 
 
 def default_path_to_raw_data(*args):
@@ -18,6 +107,17 @@ def default_path_to_raw_data(*args):
 def default_output_dir(*args):
     default_path_to_output_directory = "/Users/GBS/Master/Pipeline/python_pipe_test/bedpe_testing"
     return os.path.join(default_path_to_output_directory, *args)
+
+def hg38_blacklist(*args):
+    grc_hg38_exclusions = pbt.BedTool("/Users/GBS/Master/reference/GCA_000001405.15_GRCh38_GRC_exclusions.bed")
+    encode_blacklist = pbt.BedTool("/Users/GBS/Master/reference/ENCFF356LFX.bed")
+    combined_blacklist = pbt.Bedtools(grc_hg38_exclusions, encode_blacklist, )
+    return grc_hg38_exclusions, encode_blacklist
+
+def hg19_blacklist(*args):
+    """ ENCODE hg19/Grc37 blacklisted regions """
+
+
 
 
 def read_hicpro_output():
@@ -44,40 +144,87 @@ def read_hicpro_output():
 
     return matrix_df, bed_df
 
+
 read_hicpro_output()
+
 
 # THIS WORKS:
 # But needs to make it able to store/process multiple cell lines and feed them into the "Processing" classes
 # Clean opp senere og lagre matrix som armatus input, kanskje skriv amratus output end-inlcusive function så TAD annotation kan gjøres
 
-def create_bedpe(bed_file, matrix_file, output_file):
-    """
-    Function to create a BEDPE file from the HiC-Pro output (BED and matrix)
-    and write it to a specified directory
-    """
 
-    # Split the function: One part for processing, another for writing to file
+# Old function:
 
-    with open(bed_file, "r") as bed, open(matrix_file, "r") as matrix, open(output_file, "w") as bedpe:
-        bed_lines = bed.readlines()
-        matrix_lines = matrix.readlines()
-        bed_dict = {}
-        for line in bed_lines:
-            line = line.strip().split("\t")
-            bed_dict[line[3]] = line
-        for line in matrix_lines:
-            line = line.strip().split("\t")
-            bedpe.write(f"{bed_dict[line[0]][0]}"
-                        f"\t{bed_dict[line[0]][1]}"
-                        f"\t{bed_dict[line[0]][2]}"
-                        f"\t{bed_dict[line[1]][0]}"
-                        f"\t{bed_dict[line[1]][1]}"
-                        f"\t{bed_dict[line[1]][2]}"
-                        f"\t{line[2]}\n")
+# def create_bedpe(bed_file, matrix_file, output_file):
+#     """
+#     Function to create a BEDPE file from the HiC-Pro output (BED and matrix)
+#     and write it to a specified directory
+#     """
+#
+#     # Split the function: One part for processing, another for writing to file
+#
+#     with open(bed_file, "r") as bed, open(matrix_file, "r") as matrix, open(output_file, "w") as bedpe:
+#         bed_lines = bed.readlines()
+#         matrix_lines = matrix.readlines()
+#         bed_dict = {}
+#         for line in bed_lines:
+#             line = line.strip().split("\t")
+#             bed_dict[line[3]] = line
+#         for line in matrix_lines:
+#             line = line.strip().split("\t")
+#             bedpe.write(f"{bed_dict[line[0]][0]}"
+#                         f"\t{bed_dict[line[0]][1]}"
+#                         f"\t{bed_dict[line[0]][2]}"
+#                         f"\t{bed_dict[line[1]][0]}"
+#                         f"\t{bed_dict[line[1]][1]}"
+#                         f"\t{bed_dict[line[1]][2]}"
+#                         f"\t{line[2]}\n")
+#
+#     return bedpe
+
+
+def make_BEDPE():
+
+    """This function takes the BED and matrix files from HiC-Pro and makes a BEDPE object"""
+
+    bedpe = []
+
+    bed_file = os.path.join(default_path_to_raw_data("chr18_50000_abs.bed"))
+    matrix_file = os.path.join(default_path_to_raw_data("chr18_50000.matrix"))
+
+    bed_lines = open(bed_file, "r").readlines()
+    matrix_lines = open(matrix_file, "r").readlines()
+
+    bed_dict = {}
+    for line in bed_lines:
+        line = line.strip().split("\t")
+        bed_dict[line[3]] = line
+    for line in matrix_lines:
+        line = line.strip().split("\t")
+        bedpe.append(f"{bed_dict[line[0]][0]}"
+                     f"\t{bed_dict[line[0]][1]}"
+                     f"\t{bed_dict[line[0]][2]}"
+                     f"\t{bed_dict[line[1]][0]}"
+                     f"\t{bed_dict[line[1]][1]}"
+                     f"\t{bed_dict[line[1]][2]}"
+                     f"\t{line[2]}\n")
 
     return bedpe
 
-create_bedpe(default_path_to_raw_data("chr18_50000_abs.bed"), default_path_to_raw_data("chr18_50000.matrix"), default_output_dir("standardized_nodes_bedpe"))
+def write_bedpe(bedpe, output_file):
+
+    """This function writes the BEDPE list to a specified directory"""
+
+    with open(output_file, "w") as bedpe_file:
+        for line in bedpe:
+            bedpe_file.write(line)
+
+    return bedpe_file
+
+write_bedpe(make_BEDPE(), default_output_dir("standardized_nodes_bedpe_writingtest1"))
+
+# This works, but I want to make it into a class, so I can make its better, but later when I have done comp-net-analysis
+
 
 
 
@@ -99,8 +246,9 @@ create_bedpe(default_path_to_raw_data("chr18_50000_abs.bed"), default_path_to_ra
 # 5. Make a function that removes the overlap and outputs a BEDPE file
 
 
-def remove_blacklisted_regions(bedpe_file, output_file):
 
+
+def remove_blacklisted_regions(bedpe_file, output_file):
     """This function takes the combined regions of the relevant reference genome
     containing unmappable regions, caps, centromeres etc. and removes the parts of the
     BEDPE file that overlap with these regions. The output is a BEDPE file"""
@@ -108,30 +256,15 @@ def remove_blacklisted_regions(bedpe_file, output_file):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # function that combines all regions of hg38 that should be excluded from the analysis
 # GRC declared regions of hg38 which contain false duplication or contamination
 # We also need Hg19 (if that is the genome we are working with, which in the Jonas data it is at least I tihnk)
-grc_hg38_exclusions = pbt.BedTool("/Users/GBS/Master/reference/GCA_000001405.15_GRCh38_GRC_exclusions.bed")
-encode_blacklist = pbt.BedTool("/Users/GBS/Master/reference/ENCFF356LFX.bed")
+# grc_hg38_exclusions = pbt.BedTool("/Users/GBS/Master/reference/GCA_000001405.15_GRCh38_GRC_exclusions.bed")
+# encode_blacklist = pbt.BedTool("/Users/GBS/Master/reference/ENCFF356LFX.bed")
 
 # find overlap betwen grc_hg38_exclusions and encode_blacklist
-overlap_test = pbt.BedTool.intersect(grc_hg38_exclusions, encode_blacklist, wa=True, wb=True)
-print(overlap_test)
+# overlap_test = pbt.BedTool.intersect(grc_hg38_exclusions, encode_blacklist, wa=True, wb=True)
+# print(overlap_test)
 
 
 # Make function that combines all overlapping "blacklisted" regions, which outputs this as a BED file?
